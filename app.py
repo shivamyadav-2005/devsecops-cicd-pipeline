e4afdc69945af147a1dccdd253d203af9c70fcec
Shivam <shivamyadav40264@gmail.com>
2026-06-11 16:19:15 +0530

    Test automatic deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevSecOps CI/CD Pipeline Running Successfully!"
+    return "Version 2 - Auto Deployment Working!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
